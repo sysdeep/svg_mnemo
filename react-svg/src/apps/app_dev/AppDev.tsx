6e04bb197d8b593df5725efec3b5e090dc081f74
Igor Nefedkin <igor.nefedkin@kaspersky.com>
2025-04-29 23:17:33 +0300

    test win box

--- react-svg/src/apps/app_dev/AppDev.tsx
+++ react-svg/src/apps/app_dev/AppDev.tsx
@@ -2,6 +2,10 @@ import { SyntheticEvent, useState } from "react";
 import MotorPage from "./pages/MotorPage";
 import DSensorPage from "./pages/DSensorPage";
 
+import "winbox/dist/css/winbox.min.css"; // required
+import "winbox/dist/css/themes/modern.min.css"; // optional
+import WinBox from "react-winbox";
+
 const pages = [
   {
     name: "views",
@@ -63,6 +67,58 @@ export default function AppDev() {
       </table>
 
       <div></div>
+      <WinBoxTest />
+      <WinBoxesTest />
+    </div>
+  );
+}
+
+function WinBoxTest() {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  return (
+    <div>
+      <button onClick={() => setIsOpen(true)}>open window</button>
+      {isOpen && (
+        <WinBox
+          width={500}
+          height={300}
+          x="center"
+          y={30}
+          // noClose={this.state.inEditing}
+          onClose={() => setIsOpen(false)}
+        >
+          <div>
+            <h1>Hello, WinBox!</h1>
+          </div>
+        </WinBox>
+      )}
+    </div>
+  );
+}
+
+function WinBoxesTest() {
+  const [windows, setWindows] = useState<number[]>([]);
+  // const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const handleClose = (force: boolean, index: number) => {
+    let state = [...windows];
+    // const index = state.findIndex((info) => info.id === id);
+    if (index === -1) return;
+    // if (state[index].onclose && state[index].onclose(force)) return true;
+    // window-specific onclose, returns true if it does not need the default close process.
+    state.splice(index, 1);
+    setTimeout(() => setWindows(state)); // to change winbox showing state while `onclose`, MUST wrap it within `setTimeout`
+  };
+
+  return (
+    <div>
+      <button onClick={() => setWindows([...windows, 1])}>add window</button>
+      {windows.map((info, i) => (
+        <WinBox key={i} id={`${i}`} onClose={(force) => handleClose(force, i)}>
+          <div>Some children: {info}</div>
+        </WinBox>
+      ))}
     </div>
   );
 }
